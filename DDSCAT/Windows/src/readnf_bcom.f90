e20cb6644afac1a27966e6193ed08677bd2054cd
Unknown <kyle.hall.2013@my.bristol.ac.uk>
2017-10-30 18:48:20 +0000

    Set up environment in Linux only, as this is the most friendly way of approaching DDSCAT

--- DDSCAT/Windows/src/readnf_bcom.f90
+++
@@ -1,6 +0,0 @@
-     MODULE READNF_BCOM
-       USE DDPRECISION,ONLY: WP
-       COMPLEX(WP),ALLOCATABLE :: &
-          CXBINC(:,:,:,:),        &
-          CXBSCA(:,:,:,:)         !
-     END MODULE READNF_BCOM
