7d49b46c17788dfec27a772f22a507e001943731
Kyle Hall (FY proj SH ) <kh13490@okotoks.phy.bris.ac.uk>
2017-11-01 11:41:54 +0000

    ADDA Literature Review Tests

---
+++ ADDA/Linux/Source/src/mpi/timing.d
@@ -0,0 +1,18 @@
+timing.o: ../timing.c ../const.h ../timing.h ../parbas.h \
+ /usr/local/include/mpi.h /usr/local/include/mpio.h \
+ /usr/local/include/mpi.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/syslimits.h \
+ /usr/include/limits.h /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h /usr/include/stdint.h \
+ /usr/include/bits/wchar.h ../comm.h ../types.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/stdbool.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/stddef.h ../function.h \
+ ../io.h /usr/include/stdio.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h ../vars.h /usr/include/time.h \
+ /usr/include/bits/time.h
