28d50fc4a51551f2f1c7e7253d1178263d442df7
Kyle Hall (FY proj SH ) <kh13490@llanilar.phy.bris.ac.uk>
2017-11-29 11:48:31 +0000

    Completed first draft of working python script for forces

--- ADDA/Linux/SimpleParticleTracking/DefaultADDA_Barton5_10Points/JobScript.py
+++ ADDA/Linux/SimpleParticleTracking/DefaultADDA_Barton5_10Points/JobScript.py
@@ -12,7 +12,7 @@ Final_y=5
 y=Initial_y #Set the value of y to the initial value
 
 while (y<Final_y):
-  callString="../adda -size 2 -lambda 1 -prop 0 0 -1 -beam barton5 1 "+str(x)+" "+str(y)+" "+str(z)+" -store_beam -store_dip_pol -store_int_field"
+  callString="../adda -size 2 -lambda 1 -prop 0 0 1 -beam barton5 1 "+str(x)+" "+str(y)+" "+str(z)+" -store_beam -store_dip_pol -store_int_field"
   print callString
   subprocess.call(callString,shell=True)
   y=y+Step_y
--- ADDA/Linux/Test/pythonscript.py
+++ ADDA/Linux/Test/pythonscript.py
@@ -11,22 +11,22 @@ from mpl_toolkits.mplot3d import axes3d
 
 
 
-def Forces(axes, DipPol, Eint):
+def Forces(axes, DipPol, Eint, DipoleSep):
     d_l = np.zeros([2])  #d for central difference formula, for each axis
     dE = np.zeros([3, 3, len(Eint[0])], dtype=complex) #Derivative at each point, for each axis
     Force = np.zeros([3, len(Eint[0])]) # Force at each point
     CurrentPosition = np.zeros([3])
     for i in range(len(axes[0])): #Iterates through each point in the file
-     
+
       #Varying through each axis
       for j in range(3):
 	ValuesAvailable=np.array([True,True]) #We assume that there are X,Y or Z Values available before and after
 	if (j==0): #Calculating dx values
 	  IndexValues=(np.where((axes[1,:]==axes[1,i])&(axes[2,:]==axes[2,i])))[0]#Finds the row numbers of all values with the same Y,Z positions (varying X)
 	elif (j==1): #Calculating dy values
-	  IndexValues=(np.where((axes[0,:]==axes[0,i])&(axes[2,:]==axes[2,i])))[0]#Finds the row numbers of all values with the same Y,Z positions (varying X)
+	  IndexValues=(np.where((axes[0,:]==axes[0,i])&(axes[2,:]==axes[2,i])))[0]#Finds the row numbers of all values with the same X,Z positions (varying Y)
 	elif (j==2): #Calculating dz values
-	  IndexValues=(np.where((axes[1,:]==axes[1,i])&(axes[0,:]==axes[0,i])))[0]#Finds the row numbers of all values with the same Y,Z positions (varying X)
+	  IndexValues=(np.where((axes[1,:]==axes[1,i])&(axes[0,:]==axes[0,i])))[0]#Finds the row numbers of all values with the same Y,X positions (varying Z)
 	try: #Attempt to find a value before
 	  d_l[0]=((np.where(IndexValues<i))[0])[-1] #The preceeding Ex, Ey and Ez values
 	except: #When no value before can be found
@@ -41,15 +41,15 @@ def Forces(axes, DipPol, Eint):
 	if ((ValuesAvailable[0]==True)&(ValuesAvailable[1]==True)):
 	  #Calculate dE_l/dj by the central difference theorem
 	  for k in range(3):
-	    dE[k,j,i]=((Eint[k,(d_l[1])]-Eint[k,(d_l[0])])/(axes[k,(d_l[1])]-axes[k,(d_l[0])]))
+	    dE[k,j,i]=((Eint[k,(d_l[1])]-Eint[k,(d_l[0])])/(DipoleSep*2))
 	elif ((ValuesAvailable[0]==True)&(ValuesAvailable[1]==False)):
 	  #Calculate dE_l/dj by using only the preceeding value
 	  for k in range(3):
-	    dE[k,j,i]=((Eint[k,i]-Eint[k,(d_l[0])])/(axes[k,i]-axes[k,(d_l[0])]))
+	    dE[k,j,i]=((Eint[k,i]-Eint[k,(d_l[0])])/(DipoleSep))
 	elif ((ValuesAvailable[0]==False)&(ValuesAvailable[1]==True)):
 	  #Calculate dE_l/dj by using only the following value
 	  for k in range(3):
-	    dE[k,j,i]=((Eint[k,(d_l[1])]-Eint[k,i])/(axes[k,(d_l[1])]-axes[k,i]))
+	    dE[k,j,i]=((Eint[k,(d_l[1])]-Eint[k,i])/(DipoleSep))
 	else:
 	  #Set (dE_x)/(dj), (dE_y)/(dj), (dE_z)/(dj) equal to zero
 	  for k in range(3):
@@ -60,32 +60,42 @@ def Forces(axes, DipPol, Eint):
 	for k in range(3): #Summating the different contributions
 	  Force[j,i]=Force[j,i]+(0.5*(np.real((DipPol[k,i])*(np.conjugate(dE[k,j,i])))))
     
-    return np.vstack([axes,Force])
+    return Force
     
-    
+# Import Dipole Polorisations    
 DipPolar = np.loadtxt('DipPol-Y', skiprows=1) #skip first row if it is a heading
 x, y, z = DipPolar[:,0], DipPolar[:,1], DipPolar[:,2]
 DipPolx = DipPolar[:,4] + 1j*DipPolar[:,5]
 DipPoly = DipPolar[:,6] + 1j*DipPolar[:,7]
 DipPolz = DipPolar[:,8] + 1j*DipPolar[:,9]
 
+# Import Internal Electric Field contributions
 Eintn = np.loadtxt('IntField-Y', skiprows=1)
 Eintx = Eintn[:,4] + 1j*Eintn[:,5]
 Einty = Eintn[:,6] + 1j*Eintn[:,7]
 Eintz = Eintn[:,8] + 1j*Eintn[:,9]
 
+# Import Electric field due to beam
+Ebeam = np.loadtxt('IncBeam-Y', skiprows=1)
+Ebeax = Ebeam[:,4] + 1j*Ebeam[:,5]
+Ebeay = Ebeam[:,6] + 1j*Ebeam[:,7]
+Ebeaz = Ebeam[:,8] + 1j*Ebeam[:,9]
+
+# Import Dipole Seperation
+DipSep = (np.loadtxt('DipSep'))+0 # To remove the array condition we add zero
+
 axis = np.vstack([x,y,z]) 
-entry = np.vstack([DipPolx, DipPoly, DipPolz]) #Dipole Polarisations
-yentr = np.vstack([Eintx, Einty, Eintz]) #Internal Fields
+DipolePolarisations = np.vstack([DipPolx, DipPoly, DipPolz]) #Dipole Polarisations
+ElectricFields = np.vstack([(Eintx+Ebeax), (Einty+Ebeay), (Eintz+Ebeaz)]) #Internal Fields
 
-#print(Forces(axis, entry, yentr))
-Quiv = Forces(axis, entry, yentr)
+CalculatedForces = Forces(axis, DipolePolarisations, ElectricFields, DipSep)
+np.savetxt('Forces',np.transpose([np.vstack([axis,CalculatedForces])]),delimiter=' ')
 
-plt.quiver(Quiv[0], Quiv[1], Quiv[3], Quiv[4])
+plt.quiver(axis[0], axis[1], CalculatedForces[0], CalculatedForces[1])
 plt.show()
 
 
-print(sum(Quiv[3]), sum(Quiv[4]), sum(Quiv[5]))
+print(sum(CalculatedForces[0]), sum(CalculatedForces[1]), sum(CalculatedForces[2]))
 
 
 
