3ca3439b4c2d502da233879d1af8e22422f02513
Unknown <kyle.hall.2013@my.bristol.ac.uk>
2018-02-07 14:17:59 +0000

    Auto stash before merge of "ADDAdevelopment" and "origin/ADDAdevelopment"
    New

--- ADDA/Linux/ThirdForceCalculations/ExponentialForm/MeasuringCorrectionFactor/27dpl/CalgaryScript.sh
+++
@@ -1,5 +0,0 @@
-#PBS -N CalculateCorrectionFactor_dpl27
-# The working directory
-cd /usersc/kh13490/Project/27dpl
-# Script goes below here
-python CalculateCorrectionFactor.py
--- ADDA/Linux/ThirdForceCalculations/ExponentialForm/MeasuringCorrectionFactor/29dpl/CalgaryScript.sh
+++
@@ -1,5 +0,0 @@
-#PBS -N CalculateCorrectionFactor_dpl29
-# The working directory
-cd /usersc/kh13490/Project/29dpl
-# Script goes below here
-python CalculateCorrectionFactor.py
