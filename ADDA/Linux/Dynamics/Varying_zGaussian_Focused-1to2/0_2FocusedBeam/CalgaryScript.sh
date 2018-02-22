2158c4640ee9039b72a8e2757097b98a9378864f
Unknown <kyle.hall.2013@my.bristol.ac.uk>
2018-02-22 16:18:36 +0000

    Submitted scripts to Calgary awaiting response for Equilibrium search (Iterator running)

---
+++ ADDA/Linux/Dynamics/Varying_zGaussian_Focused-1to2/0_2FocusedBeam/CalgaryScript.sh
@@ -0,0 +1,5 @@
+#PBS -N zGaussian_0_2
+# The working directory
+cd /usersc/kh13490/Project/zGaussian_0_2
+# Script goes below here
+python CalculateDDAForces_Third_MovingBeam.py
---
+++ ADDA/Linux/FinalResults/Sphere/EquilibriumPosition/BeamWidth_0_3/CalgaryScript.sh
@@ -0,0 +1,5 @@
+#PBS -N EquilibriumIterator_0_3
+# The working directory
+cd /usersc/kh13490/Project/EquilibriumIterator_0_3
+# Script goes below here
+python IterativeEquilibriumfinder.py
