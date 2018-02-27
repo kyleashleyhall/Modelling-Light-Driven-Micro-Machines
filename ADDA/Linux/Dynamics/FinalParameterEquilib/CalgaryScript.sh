990d0fb465b58d05e703b6c2bf75de06ce221d3f
Marius <mv15885@my.bristol.ac.uk>
2018-02-27 15:51:05 +0000

    More dynamics

---
+++ ADDA/Linux/Dynamics/FinalParameterEquilib/CalgaryScript.sh
@@ -0,0 +1,5 @@
+#PBS -N DynamicParticleAtEquilib
+# The working directory
+cd /usersc/mv15885/Project/DynamicParticleAtEquilib
+# Script goes below here
+python DynamicScript.py
---
+++ ADDA/Linux/Dynamics/FinalParameters000/CalgaryScript.sh
@@ -0,0 +1,5 @@
+#PBS -N DynamicParticleAtZero
+# The working directory
+cd /usersc/mv15885/Project/DynamicParticleAtZero
+# Script goes below here
+python DynamicScript.py
