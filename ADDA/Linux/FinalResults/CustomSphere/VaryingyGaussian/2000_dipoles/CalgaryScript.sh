d69df5bb7ee588bac6aed6302d30d9e4e9b9d754
Unknown <kyle.hall.2013@my.bristol.ac.uk>
2018-03-04 13:39:46 +0000

    Updated working yGaussian scripts and Correction Factors. Waiting for the yGaussian for Sphere and Corrected Sphere to dictate the progression of the project

--- ADDA/Linux/FinalResults/CustomSphere/VaryingyGaussian/2000_dipoles/CalgaryScript.sh
+++ ADDA/Linux/FinalResults/CustomSphere/VaryingyGaussian/2000_dipoles/CalgaryScript.sh
@@ -1,5 +1,5 @@
-#PBS -N yGaussian_0_25_20000
+#PBS -N CustomSphere_yGaussian_0_25_20000
 # The working directory
-cd /usersc/kh13490/Project/yGaussian_0_25
+cd /usersc/kh13490/Project/CustomSphere_yGaussian_0_25_20000
 # Script goes below here
 python CalculateDDAForces_Third_MovingBeam.py
---
+++ ADDA/Linux/FinalResults/Sphere/VaryingyGaussian/CalgaryScript.sh
@@ -0,0 +1,5 @@
+#PBS -N ADDASphere_yGaussian_0_25
+# The working directory
+cd /usersc/kh13490/Project/ADDASphere_yGaussian_0_25
+# Script goes below here
+python CalculateDDAForces_Third_MovingBeam.py
