0b03be2c12817bf599ee4f718f98673b192dd7d5
Marius <mv15885@my.bristol.ac.uk>
2018-03-09 15:32:57 +0000

    Brownian verification and twin beam dynamics for size 1

---
+++ ADDA/Linux/Dynamics/TwoBeams/Brownian/SepertationVariations/ASep20/Size1/MonsterScript.sh
@@ -0,0 +1,5 @@
+#PBS -N TBDynamicsSep2Size1
+# The working directory
+cd /home/mv15885/Project/HoppingSep2Size1
+# Script goes below here
+python 2BeamDynamicsS1.py
---
+++ ADDA/Linux/FinalResults/Sphere/TwinBeam/Varying_yGaussian/beamwidth1/Sep2/Size1/DisplayExperimentForces.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 10 13:57:26 2017
+
+@author: Marius and Kyle
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+Forces = np.transpose(np.loadtxt('ForcesY'))
+
+plt.figure(1)
+
+plt.subplot(311)
+plt.plot(Forces[0],Forces[1])
+
+plt.subplot(312)
+plt.plot(Forces[0], np.zeros((len(Forces[0]))))
+plt.plot(Forces[0],Forces[2])
+
+plt.subplot(313)
+plt.plot(Forces[0],Forces[3])
+
+plt.show()
