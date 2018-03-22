c0981be7738b89070c1eeac6061bf2795f1c3b28
Unknown <kyle.hall.2013@my.bristol.ac.uk>
2018-03-22 12:11:29 +0000

    Some more stuff

---
+++ ADDA/Linux/Dynamics/SingleBeam_zDynamics/0_25BeamWidth/NonDynamic/ParticleSize_0_2/MonsterScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#PBS -N Sphere_zDynamics_NonDynamic_ParticleSize_0_2
+# The working directory
+cd /home/kh13490/Project/Sphere_zDynamics_NonDynamic_ParticleSize_0_2
+# Script goes below here
+python CalculateDDAForces_Third_MovingBeam.py
---
+++ ADDA/Linux/Dynamics/SingleBeam_zDynamics/0_25BeamWidth/NonDynamic/ParticleSize_2_0/MonsterScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#PBS -N Sphere_zDynamics_NonDynamic_ParticleSize_2_0
+# The working directory
+cd /home/kh13490/Project/Sphere_zDynamics_NonDynamic_ParticleSize_2_0
+# Script goes below here
+python CalculateDDAForces_Third_MovingBeam.py
